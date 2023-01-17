0e2b179ba07340791cd383d0c0ef7c3eba7e219f
”vladimir817vk@gmail.com” <vladimir817vk@gmail.com>
2023-01-17 23:52:19 +0300

    add storybooksource

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,7 +7,22 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
+    "@storybook/preset-create-react-app",
+    {
+      name: '@storybook/addon-storysource',
+      options: {
+        rule: {
+          test: [/\.stories\.jsx?$/],
+        },
+        loaderOptions: {
+          prettierConfig: {
+            printWidth: 80, singleQuote: false,
+            options: {parser: 'typescript'}
+          },
+          injectStoryParameters: false,
+        },
+      },
+    }
   ],
   "framework": "@storybook/react",
   "core": {
